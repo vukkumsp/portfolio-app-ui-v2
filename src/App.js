6c45a27511d6cf418dac0456dddf15e6a0386649
V. Sai Prakash <vukkumsp@gmail.com>
2022-05-28 10:04:22 +0530

    basic structure done;

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 // Layout Components
-
-// Pages
-import Home from './pages/Home/Home';
-import PrimarySkills from './pages/PrimarySkills/PrimarySkills';
-import SecondarySkills from './pages/SecondarySkills/SecondarySkills';
-
+import Header from './layout/Header/Header';
+import Body from './layout/Body/Body';
+import Footer from './layout/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Home></Home>
-      <PrimarySkills></PrimarySkills>
-      <SecondarySkills></SecondarySkills>
+      <Body></Body>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/layout/Body/Body.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Body.module.css';
+
+// Pages
+import Home from './../../pages/Home/Home';
+import PrimarySkills from './../../pages/PrimarySkills/PrimarySkills';
+import SecondarySkills from './../../pages/SecondarySkills/SecondarySkills';
+
+const Body = () => (
+  <div className={styles.Body} data-testid="Body">
+    Body Component
+    <Home></Home>
+    <PrimarySkills></PrimarySkills>
+    <SecondarySkills></SecondarySkills>
+  </div>
+);
+
+Body.propTypes = {};
+
+Body.defaultProps = {};
+
+export default Body;
