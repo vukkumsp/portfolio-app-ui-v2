d05f0e849cf4790769a0719369c32fb9df209d9f
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2024-12-04 21:31:19 +0530

    added data/contact.json to test if it is possible to use external data file;

--- src/Components/Heading/Heading.jsx
+++ src/Components/Heading/Heading.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import './Heading.css';
+import { useEffect } from 'react';
+// import role from '/data/content.json';
+
 
 function Heading() {
+    const [role, setRole] = useState(null);
+    useEffect(()=>{
+        fetch('/data/content.json')
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.role);
+          setRole(data.role);
+        })
+        .catch((error) => console.error('Error fetching JSON:', error));
+    });
+
     return (
     <div className='heading'>
         <p className='location'>
@@ -10,7 +25,8 @@ function Heading() {
             I am <span className='name'>Sai Prakash</span>,
         </div>
         <div>
-            a <span className="job-role">Frontend Developer</span>
+            a <span className="job-role">{role}</span>
+            
         </div>
     </div>
     );
